5912fd5a7fde3eb115777f99e114f883fda3e849
ShankShanon <85640596+ShankShanon@users.noreply.github.com>
2021-08-30 11:21:01 +0600

    null vs undefined

---
+++ nullVSundefined.js
@@ -0,0 +1,23 @@
+// let pakhi;
+// console.log(pakhi);
+
+// function add(num1, num2){
+//     console.log( num1 + num2);
+
+// }
+
+// const result = add(12, 82);
+// console.log(result);
+
+// function add( num1, num2 = 10){
+//     results = (num2 + num1);
+//     return results;
+// }
+// const result = add(12);
+// console.log(result);
+
+// const lover = {name:"Shank", phone:0197647365};
+// console.log(lover.gf);
+
+let fun = undefined;
+console.log(fun);
