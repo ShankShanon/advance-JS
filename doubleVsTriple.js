3b5d46b5da44835ca20feaf11f8939794f7f860b
ShankShanon <85640596+ShankShanon@users.noreply.github.com>
2021-08-30 13:34:57 +0600

    mapping and filtering

---
+++ doubleVsTriple.js
@@ -0,0 +1,19 @@
+const first = 1;
+const second = true;
+
+if(first == second){
+    console.log("condition is true")
+}
+else{
+    console.log("condition is false")
+}
+
+const x = 1;
+const y = true;
+
+if(x === y){
+    console.log("condition is true")
+}
+else{
+    console.log("condition is false")
+}
